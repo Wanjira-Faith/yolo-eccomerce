523bef9733d5b35e6b1708bfc9a4b8b0c02804ec
Wanjira-Faith <wanjirafaith22@gmail.com>
2024-12-01 16:49:49 +0300

    updated file

--- src/assets/fake-data/hero-slider.js
+++ src/assets/fake-data/hero-slider.js
@@ -8,24 +8,24 @@ const heroSliderData = [
         description: "When it comes to luxury, the pima fabric is a must-mention. It provides the best quality for any fashion product. Pima yarn is twice as long and thick as regular cotton yarn due to advanced weaving technology. This makes the polo shirt more durable, prevents pilling, and ensures softness, color retention, and is very safe for one's health.",
         img: img1,
         color: "blue",
-        slug: "women-polo-shirt-01", 
-        // path: "/catalog/women-polo-shirt-01" 
+        slug: "polo-shirt-04", 
+        path: "/catalog/polo-shirt-04" 
     },
     {
         title: "Modal Loose Fit Women's Polo",
         description: "The Modal Loose Fit Women's Polo uses eco-friendly fabric technology that prevents shrinkage. This modal fabric polo shirt is ideal for those working in dynamic environments today.",
         img: img2,
         color: "pink",
-        slug: "women-polo-shirt-02", 
-        path: "/catalog/women-polo-shirt-02"
+        slug: "polo-shirt-05", 
+        path: "/catalog/polo-shirt-05"
     },
     {
         title: "Coolmax Lacoste Women's Polo",
         description: "This women's polo shirt is made from Coolmax fabric, providing a cool and refreshing feel when worn. The design of this Coolmax polo is incredibly comfortable and creates a sleek appearance, making it a hot item for the upcoming season.",
         img: img3,
         color: "orange",
-        slug: "women-polo-shirt-03", 
-        path: "/catalog/women-polo-shirt-03"
+        slug: "polo-shirt-06", 
+        path: "/catalog/polo-shirt-06"
     }
 ];
 
